4a51628bb9c59c3c2b223a78dc58ce08628f23b6
j025619kimo <j025619kimo@hotmail.com>
2023-10-31 17:13:05 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,59 @@
+function getUrlParms(){ //取得網頁參數
+    var args = new Object();
+    var url = location.search.substring(1);
+    var pairs = url.split("&");
+    for(var i=0; i<pairs.length; i++) {
+        var pos = pairs[i].indexOf('='); // 找到每個name value
+        if(pos==-1)   continue; //如果沒有就跳過
+        var argname = pairs[i].substring(0,pos); //取得name
+        var value = pairs[i].substring(pos+1); //取得value
+        args[argname] = decodeURI(value); //把取到的name value丟到此陣列去
+    }
+    return args;
+}
+
+var theDate;
+var args = new Object();
+    args = getUrlParms();
+    if (args["date"]!=undefined) {
+        theDate = args["date"];
+    }
+console.log(theDate);
+
+$('.kvSlider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
+
+
+$(document).ready(function(){
+
+    $('.ppscroll').scrollParallax();
+
+    $('#openEnvelope').on('click',function(){
+        $('#envelopeModal').modal('show');
+        return false;
+    });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() < 200) {
+            if ($("#goTop").hasClass("show")) {
+                $("#goTop").toggleClass("show");
+            }
+        } else {
+            $("#goTop").addClass("show");
+        }
+    });
+    $('#goTop').on('click',function(){
+        $('html,body').animate({
+            scrollTop: 0
+        },600);
+    });
+
+});
--- js/scroll.js
+++ js/scroll.js
@@ -8,6 +8,46 @@ function stopMobileMagic(magic,width){
 
 
 
+var timeline_sCe = new TimelineMax();
+timeline_sCe.staggerFromTo('.comicEndText', 1, 
+                            {x: 1500,rotation:180},
+                            {x: 0,rotation:0, ease: Back.easeOut}
+                            ,0.15)
+            .from('#comicEndPic', 1, {y: '+=100px', autoAlpha: 0})
+            .from('#g3441', 4, {x: '-=100%'},3)
+            .from('#g3441 p', 1, {autoAlpha: 0},3);
+var scene_sCe = new ScrollMagic
+                .Scene({
+                    triggerElement: "#sCe",
+                    triggerHook: "onEnter",
+                    duration: '85%', 
+                    offset: "1000%"
+                }).on("progress", function(e) {
+                    var o = Math.floor(100 * e.progress);
+                    if (o>50) {
+                        $('.comicEndTitle').addClass('active');
+                    }else {
+                        $('.comicEndTitle').removeClass('active');
+                    }
+                })
+                .setTween(timeline_sCe)
+                .addTo(controller);
+
+var timeline_sAro = new TimelineMax();
+timeline_sAro.from('.aroundMid', 1, {scale: 3,autoAlpha: 0, ease:Power4.easeInOut})
+           .from('#iphone1', 1, {y: '-=800px', ease:Power4.easeInOut},0)
+           .from('#iphone2', 1, {x: '+=600px', ease:Power4.easeInOut},0)
+           .from('#iphone3', 1, {x: '-=600px', ease:Power4.easeInOut},0)
+           .from('#iphone4', 1, {y: '+=800px', ease:Power4.easeInOut},0);
+var scene_sAro = new ScrollMagic
+                    .Scene({
+                        triggerElement: "#sAro",
+                        duration: "50%", 
+                        offset: "50%"
+                    })
+                    .setTween(timeline_sAro)
+                    .addTo(controller);
+
 var timeline_sEnv = new TimelineMax();
 timeline_sEnv.staggerFrom('.envelopePuzzple', 1, {y: '+=1000px'},0.5)
             .to('.envelopePuzzple', 1, {autoAlpha: 0},"<")
